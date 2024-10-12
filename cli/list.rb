6285b217bf8a81b82737600873f3545daaf0622d
leandrosardi <leandro.sardi@expandedventure.com>
2024-10-12 13:46:23 -0300

    coding

--- cli/list.rb
+++ cli/list.rb
@@ -24,39 +24,76 @@ begin
         name_search: x,
         logger: l
     )
-
-    # Define the table rows
-    rows = []
-    rows << [
-        'Name'.bold, 
-        'IP'.bold, 
-        'Contabo'.bold, 
-        'Expire'.bold,
-    ]
     
-    all.each { |j|
-        ip = j[:node][:ip] if j[:node]
-        ip = j[:instance].dig('ipConfig', 'v4', 'ip') if ip.nil? && j[:instance]
+    while (true)
+        # clean the rows array
+        rows = [] 
+
+        # Define the table rows
         rows << [
-            j[:node].nil? ? '-' : j[:node][:name], 
-            ip, 
-            j[:instance].nil? ? '-' : j[:instance]['name'], 
-            j[:instance].nil? ? '-' : j[:instance]['cancelDate']
+            'Name'.bold, 
+            'IP'.ljust(15).bold, 
+            'Contabo'.ljust(12).bold, 
+            'Expire'.rjust(12).bold,
+            'Braanch'.ljust(10).bold,
+            'RAM'.rjust(10).bold,
+            'CPU'.rjust(10).bold,
+            'Disk'.rjust(10).bold,
+            'Alerts'.rjust(10).bold,
+            'Status'.ljust(10).bold, 
         ]
-    }
 
-    # Create the table with a title
-    table = Terminal::Table.new :title => "Nodes List", :rows => rows do |t|
-        t.style = {
-            border_x: '',  # Horizontal border character
-            border_y: '',  # Vertical border character
-            border_i: ''   # Intersection character
+        all.each { |j|
+            ip = j[:node][:ip] if j[:node]
+            ip = j[:instance].dig('ipConfig', 'v4', 'ip') if ip.nil? && j[:instance]
+            
+            branch = j[:node] ? j[:node][:git_branch] : '-'
+            ram = j[:node] ? 'connecting...'.yellow : '-'
+
+            rows << [
+                j[:node].nil? ? '-' : j[:node][:name], 
+                ip, 
+                j[:instance].nil? ? '-' : j[:instance]['name'], 
+                j[:instance].nil? ? '-' : j[:instance]['cancelDate'],
+                branch,
+                '', # ram
+                '', # cpu
+                '', # disk
+                '', # alerts
+                'connecting...'.yellow,
+            ]
         }
-    end
-    
-    # Display the table in the terminal
-    puts table
-    
+
+        # Create the table with a title
+        table = Terminal::Table.new(:rows => rows) do |t|
+            t.style = {
+                border_x: '',  # Horizontal border character
+                border_y: '',  # Vertical border character
+                border_i: ''   # Intersection character
+            }
+
+            # Set column widths (e.g., 20, 15, 25, 15, 10, 20)
+            #t.column_widths = [20, 15, 25, 15, 10, 20]
+
+            # Align columns: 0 for left, 1 for center, 2 for right
+            t.align_column(0, :left)    # First column: left-aligned
+            t.align_column(1, :left)  # Second column: center-aligned
+            t.align_column(2, :left)    # Third column: left-aligned
+            t.align_column(3, :right)   # Fourth column: right-aligned
+            t.align_column(4, :left)  # Fifth column: center-aligned
+            t.align_column(5, :right)   # Sixth column: right-aligned
+            t.align_column(6, :right)
+            t.align_column(7, :right)
+            t.align_column(8, :right)
+            t.align_column(9, :left)
+        end
+        
+        # Display the table in the terminal
+        system('clear')
+        puts table
+        sleep(10)
+    end # while (true)
+
 rescue => e
     l.reset
     l.log(e.to_console.red)
