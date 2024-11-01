24b1f05fa0e7e0e84e6d1f182b7dab4147a172ba
leandrosardi <leandro.sardi@expandedventure.com>
2024-11-01 18:56:59 -0300

    coding

--- cli/source.rb
+++ cli/source.rb
@@ -3,8 +3,17 @@ require_relative '../config.rb'
 
 l = BlackStack::LocalLogger.new('blackops.log')
 
+# Check if a script name is provided
+if ARGV.empty? || ARGV[0] == '--help' || ARGV[0] == '--h' || ARGV[0] == '-h'
+    puts 'This command opens an SSH connection with a node defined in your configuration file.'
+    puts "Usage: ops ssh <node name>"
+    exit 1
+end
+  
+node_name = ARGV.shift # Get the name of the node to connect
+
 begin
-    BlackOps.source( :master,
+    BlackOps.source_remote( node_name.to_sym,
         op: :hostname,
         connect_as_root: true,
         logger: l
