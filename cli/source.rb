733acaf41765ce5e5984b5e678f3c5731b2323aa
leandrosardi <leandro.sardi@expandedventure.com>
2024-11-02 09:54:26 -0300

    coding

--- cli/source.rb
+++ cli/source.rb
@@ -4,18 +4,21 @@ require_relative '../config.rb'
 l = BlackStack::LocalLogger.new('blackops.log')
 
 begin
-=begin
-    BlackOps.source_remote( :w01a,
-        op: :'mysaas.install.ubuntu_20_04.base',
-        connect_as_root: true,
-        logger: l
-    )
-=end
-    BlackOps.source_remote( :w01a,
-        op: :'mysaas.install.ubuntu_20_04.adspower',
-        connect_as_root: true,
-        logger: l
-    )
+    [:w01b, :w01c, :w01d, :w01e, :w01f].each { |s|
+        l.logs "#{s.to_s}... "
+        BlackOps.source_remote( s.to_sym,
+            op: :'mysaas.install.ubuntu_20_04.base',
+            connect_as_root: true,
+            logger: l
+        )
+
+        BlackOps.source_remote( s.to_sym,
+            op: :'mysaas.install.ubuntu_20_04.adspower',
+            connect_as_root: true,
+            logger: l
+        )
+        l.done
+    }
 rescue => e
     l.reset
     l.log(e.to_console.red)
