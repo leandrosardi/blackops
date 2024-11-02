696c21c2b1e47923415ee7c2a12d39f0c7a4288f
leandrosardi <leandro.sardi@expandedventure.com>
2024-11-02 09:38:09 -0300

    coding

--- cli/source.rb
+++ cli/source.rb
@@ -4,18 +4,18 @@ require_relative '../config.rb'
 l = BlackStack::LocalLogger.new('blackops.log')
 
 begin
+=begin
     BlackOps.source_remote( :w01a,
         op: :'mysaas.install.ubuntu_20_04.base',
         connect_as_root: true,
         logger: l
     )
-
+=end
     BlackOps.source_remote( :w01a,
         op: :'mysaas.install.ubuntu_20_04.adspower',
         connect_as_root: true,
         logger: l
     )
-
 rescue => e
     l.reset
     l.log(e.to_console.red)
