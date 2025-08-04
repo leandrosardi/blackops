ff9e15a2c06bf6fbec76004f09212b07bade5e95
leandrosardi <leandro@massprospecting.com>
2025-08-04 14:05:09 -0300

    https://github.com/MassProspecting/docs/issues/724

--- service/monitoring_merge.service.sh
+++ service/monitoring_merge.service.sh
@@ -6,8 +6,11 @@ source /etc/profile.d/rvm.sh
 # Use the default Ruby version
 rvm --default use 3.1.2
 
+# Set the RUBYLIB environment variable
+export RUBYLIB=/home/!!ssh_username/code1/!!code_folder
+
 # Change to the application directory
-cd /home/!!ssh_username/code1/!!code_folder/extensions/monitoring/p
+cd /home/!!ssh_username/code1/!!code_folder
 
 # Execute the Ruby application
-exec ruby ./merge.rb
+exec ruby ./extensions/monitoring/p/merge.rb
